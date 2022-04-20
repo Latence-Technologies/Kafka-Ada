46c3820209c42b135e095a8b19469876dbc79ea5
Alexander Winter <8527540+WinterAlexander@users.noreply.github.com>
2022-04-19 20:33:48 -0400

    Update wrapper.c

--- src/wrapper.c
+++ src/wrapper.c
@@ -1,6 +1,8 @@
 
 #include <librdkafka/rdkafka.h>
 
+// We have to do this because of https://github.com/edenhill/librdkafka/issues/2822
+// to support older versions of librdkafka (e.g. Ubuntu 20)
 RD_EXPORT const char* rd_kafka_message_errstr_wrapper(const rd_kafka_message_t* rkmessage)
 {
     return rd_kafka_message_errstr(rkmessage);
